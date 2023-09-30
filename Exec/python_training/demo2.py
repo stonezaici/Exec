93783b5ce5887d1394a4a2e9b4ae07efd8788c3a
Zhang, Allen Lei <stonezaici@gmail.com>
2023-09-30 18:16:58 +0800

    test commit

---
+++ Exec/python_training/demo2.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# @ProjName  :Exec
+# @FileName  :demo2.py
+# @Time      :2023-2023/9/30-17:57
+# @Author    :stone
+'''
+project description: this is demo2.py.
+'''
+
+if __name__ == "__main__":
+    run_code = 0
